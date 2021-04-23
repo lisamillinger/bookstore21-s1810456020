acba985d5fec22d4ef0905eda353650f92eba87b
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-04-23 09:19:16 +0200

    5 files modified

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,17 +1,18 @@
-import { Component, EventEmitter, OnInit, Output } from "@angular/core";
-import { Book, Image } from "../shared/book";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Book, Image, Author } from "../shared/book";
 import { BookStoreService } from "../shared/book-store.service";
+
 @Component({
   selector: "bs-book-list",
   templateUrl: "./book-list.component.html",
-  styles: []
+  styleUrls: []
 })
 export class BookListComponent implements OnInit {
   books: Book[];
 
   constructor(private bs: BookStoreService) {}
-  ngOnInit() {
-    //asynchron
+
+  ngOnInit(): void {
     this.bs.getAll().subscribe(res => (this.books = res));
     console.log("observer registered");
   }
